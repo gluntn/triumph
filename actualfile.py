566dd43a3bf29690d59e26911bd390a7c2e05ba0
William <wostensen96@gmail.com>
2016-04-13 18:07:06 +0200

    This is my first programming

---
+++ actualfile.py
@@ -0,0 +1,9 @@
+# Hello, this is a simple program
+# It will ask your name
+# and your age
+# and do nothing about it
+
+name = raw_input("What is your name, my loved one: ")
+age = raw_input("Then what is your age, %s? " % name)
+
+print "thx" 
